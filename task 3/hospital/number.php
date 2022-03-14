fbc5dd8e632fe204dd3a2bf20503f630212a84ed
Esraa <76266985+Soo380@users.noreply.github.com>
2022-03-15 00:21:48 +0200

    Update number.php

--- task 3/hospital/number.php
+++ task 3/hospital/number.php
@@ -1,30 +1,57 @@
 <?php
-$title = "number";
-include_once "header.php";
+    // $title = "Number";
+    // include_once "header.php";
 
-if ($_POST) {
-    $phone = $_POST['phone'];
-    //echo $phone;
-}
+    if($_POST){
+        // Validation
+        $_SESSION['phone'] = $_POST['phone'];
+        
+        $errors = [];
+        if(empty($_POST['phone'])){
+             $errors['phone-required'] = "<div class='text-danger my-1'> * Phone Number is Required </div>";
+        }
+    }
 ?>
-<div class="container">
-    <div class="row justify-content-center mt-1">
-        <div class="col-12 text-center text-dark h1 font-weight-bold">
-            <?= $title ?>
-        </div>
-        <div class="col-5 mt-4">
-            <form action="review.php" method="post">
-                <div class="form-group w-50">
-                    <label for="phone">Phone Number</label>
-                    <input type="text" name="phone" id="phone" class="form-control" placeholder="Enter Your Phone Number" aria-describedby="helpId">
-                </div>
-                <div class="form-group">
-                    <button class="mt-0 btn btn-outline-dark rounded">Send</button>
-                </div>
-            </form>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>hospital</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-1">
+            <div class="col-5 mt-4">
+                <form action="review.php" method="post">
+                    <div class="form-group w-50">
+                        <label for="phone">Phone Number</label>
+                        <input type="number" name="phone" id="phone" class="form-control" placeholder="Enter Your Phone Number" aria-describedby="helpId">
+                       <?php 
+                        if(isset($errors['phone-required'])){
+                            echo $errors['phone-required'];
+                        }
+                    ?>
+                    </div>
+                    <div class="form-group">
+                        <button class="mt-0 btn btn-outline-dark rounded">Send</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
-<?php
-include_once "footer.php";
-?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
